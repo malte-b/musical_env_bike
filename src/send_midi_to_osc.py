e36952cc9d9e361f02d21ebae2ac3ab3625eb5cb
carlaterboven <carla.terboven@student.hpi.de>
2021-01-19 12:55:44 +0100

    Add 16 sound generators to pd patch

--- src/send_midi_to_osc.py
+++
@@ -1,7 +0,0 @@
-# Set up  client for testing pd patch
-from pythonosc.udp_client import SimpleUDPClient
-
-client = SimpleUDPClient("127.0.0.1", 6666)
-
-# Send message with frequency
-client.send_message("/oscillator/frequency", 300)
--- src/send_osc_to_pd.py
+++ src/send_osc_to_pd.py
@@ -4,4 +4,10 @@ from pythonosc.udp_client import SimpleUDPClient
 client = SimpleUDPClient("127.0.0.1", 6666)
 
 # Send message with frequency
-client.send_message("/oscillator/frequency", 300)
+# client.send_message("/oscillator/frequency", 300)
+
+# Send message with MIDI note
+# client.send_message("/midi", 64)
+
+# Send message to activate one of the 16 sound generators
+client.send_message("/select", 0)
